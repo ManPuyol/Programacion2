75112624c66211ee6b3c0c3311229ea2c0d4f4fa
Manuehh <manuel.ortega@gmail.com>
2021-02-23 13:08:47 +0100

    test de funcion

--- Ejemplo 1/src/Ejemplo.java
+++ Ejemplo 1/src/Ejemplo.java
@@ -1,34 +1,45 @@
-public class Ejemplo{
-   public static void main(String[] args){
-      Asignatura as = new Asignatura("Programación II",5);
-      
-      Alumno a = new Alumno("Manolo", "Pérez", "56789890z",5f);      
-      as.addAlumno(a);
-      
-      a = new Alumno("María", "García", "87415268t",6f);           
-      as.addAlumno(a);
-      
-      a = new Alumno("Juan José","Torres", "96354712d",6f);           
-      as.addAlumno(a);
-      
-      a = new Alumno("Laura", "Meseguer", "87415268t",6f);           
-      as.addAlumno(a);
-      
-      a = new Alumno("Lucía", "Serra", "77441166h",5f);           
-      as.addAlumno(a);
-      
-      a = new Alumno("Antonio", "Peris", "96358425A",5.5f);           
-      as.addAlumno(a);
-      
-       
-      
-      
-      System.out.println("Nombre de la asignatura: " + as.getNombre()); 
-      System.out.println("Nota Media:              " + as.notaMedia());
-      a = as.getAlumMax();
-      System.out.println("El alumno/a " + a.getNombre() + " " + a.getApellidos() + " ha obtenido la máxima nota: " + a.getNota());
-      a = as.getAlumMin();
-      System.out.println("El alumno/a " + a.getNombre() + " " + a.getApellidos() + " ha obtenido la mínima nota: " + a.getNota());
-      
-   }
+public class Ejemplo {
+
+  public static void main(String[] args) {
+    Asignatura as = new Asignatura("Programaciï¿½n II", 5);
+
+    Alumno a = new Alumno("Manolo", "Pï¿½rez", "56789890z", 5f);
+    as.addAlumno(a);
+
+    a = new Alumno("Marï¿½a", "Garcï¿½a", "87415268t", 8f);
+    as.addAlumno(a);
+
+    a = new Alumno("Juan Josï¿½", "Torres", "96354712d", 6f);
+    as.addAlumno(a);
+
+    a = new Alumno("Laura", "Meseguer", "87415268t", 6f);
+    as.addAlumno(a);
+
+    a = new Alumno("Lucï¿½a", "Serra", "77441166h", 4f);
+    as.addAlumno(a);
+
+    a = new Alumno("Antonio", "Peris", "96358425A", 5.5f);
+    as.addAlumno(a);
+
+    System.out.println("Nombre de la asignatura: " + as.getNombre());
+    System.out.println("Nota Media:              " + as.notaMedia());
+    a = as.getAlumMax();
+    System.out.println(
+      "El alumno/a " +
+      a.getNombre() +
+      " " +
+      a.getApellidos() +
+      " ha obtenido la mï¿½xima nota: " +
+      a.getNota()
+    );
+    a = as.getAlumMin();
+    System.out.println(
+      "El alumno/a " +
+      a.getNombre() +
+      " " +
+      a.getApellidos() +
+      " ha obtenido la mï¿½nima nota: " +
+      a.getNota()
+    );
+  }
 }
